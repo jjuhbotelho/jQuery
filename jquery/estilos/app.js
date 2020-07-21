07c95e1753a1fcf4ad9a682e91da697a886d84a3
Júlia Botelho <juhbotelho.contato@gmail.com>
2020-07-21 18:05:55 -0300

    Finalizando manipulaçao de CSS com jQuery

--- jquery/estilos/app.js
+++ jquery/estilos/app.js
@@ -1,6 +1,6 @@
-/* Pega o valor ou define uma propriedade CSS*/
+/* Pega o valor ou define uma propriedade CSS
 var psize = $('.modal-content').css('background-color' , '#FF0000');
-console.log(psize);
+console.log(psize);*/
 /* Podemos definir mais de uma propriedade 
 $('.modal').css({
     'background-color': 'blue',
@@ -10,7 +10,7 @@ $('.modal').css({
 /* Vamos criar uma função que não entendi ainda 
 $(document).scroll(function(){
    var documentTop = $(this).scrollTop ();
-   $('.modal').css('height', documentTop * 10);
+   $('.modal').css('height', documentTop);
 });*/
 /* Pegam ou definem o valor de height (width vai ser o mesmo) */
 var height = $('.modal').height();
@@ -22,5 +22,25 @@ console.log(innerHeight)
 var outerHeight = $('.modal').outerHeight(true);
 console.log(outerHeight);
 
-var windowHeight = $(window).height();
-$('.modal').height(windowHeight);
+//var windowHeight = $(window).height();
+//$('.modal').height(windowHeight);
+
+/* Retorna um objeto com as distâncias de top e left do elemento em relação ao documento 
+
+var offset = $('.modal').offset().left;
+console.log(offset);*/
+
+/* Definir coordenadas de top e left */
+$('.modal').offset({top: 200 , left: 300 });
+
+/* Pega o topo do elemento em relação ao seu scroll */
+$(window).scroll(function(){
+    var scrollTop  = $(this).scrollTop();
+    console.log(scrollTop);
+});
+
+/*  */
+$('.btn').click(function(e){
+    e.preventDefault();
+    $(window).scrollTop(0);
+})
