4b4876e81eec04865f30510aa726869f55bc55a7
Julia <juhbotelho.contato@gmail.com>
2020-08-10 19:05:19 -0300

    Menu de paginas animado

--- pratico/slide/js/app.js
+++ pratico/slide/js/app.js
@@ -97,8 +97,28 @@ function slider(sliderName , velocidade){
 
 slider('introducao' , 500);
 
+//Animação dos botões de menu com Scroll
 
+var $target = $('[data-anime="scroll"]'),
+	animationClass = 'animate',
+	offset = $(window).height() * 3/4; 
 
+function animeScroll(){
+	var documentTop = $(document).scrollTop();
 
+	$target.each(function(){
+		var itemTop = $(this).offset().top;
+
+		if (documentTop > itemTop - offset){
+			$(this).addClass(animationClass);
+		} else {
+			$(this).removeClass(animationClass);
+		}	
+	});
+}
+
+$(document).scroll(function(){
+	animeScroll();
+});
 
 
